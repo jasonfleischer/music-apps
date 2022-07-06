efd7a0e20bd6f255e4b3a2f9c9fdd59e118e0b1e
Jason Fleischer <jason_fleischer@hotmail.ca>
2022-07-06 19:58:18 -0400

    update

--- js/main.js
+++ js/main.js
@@ -12,10 +12,31 @@ dismissInfo = function(){
 init = function() {
 
 	alert.init();
+	setup_keyboard_listeners();
 
 	var isSafariMobile = window.mobileAndTabletCheck() && isSafari;
 	if (isSafariMobile && !isFromHomeScreen()){
 		install.showAlert();
 	}
-
 }
+
+function setup_keyboard_listeners() {
+	window.onkeydown = function(e) {
+		return e.keyCode !== 32 && e.key !== " ";
+	};
+	document.addEventListener('keyup', function(event){
+		var code = event.code;
+		if (code === 'KeyA') {
+			openAll()
+		} else {
+			event.preventDefault();
+		}
+		function openAll(){
+			var sites = ["metronome", "eartrainer", "scales", "synth", "tuner", "just-intonation"];
+			for(let i in sites){
+				window.open("https://jasonfleischer.github.io/"+sites[i], '_blank');
+			}
+		}
+	})
+}
+
